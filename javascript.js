2021dd21cea1153b0d9d54121664aab215cb7f8e
pierifrancia  <pieri.francia@gmail.com>
2019-10-22 01:28:51 -0300

    ej de clase

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let saludo = "Hola, chicas";
+
+let nombre = "Pierina";
+
+let bienvenida = "Bienvenida ";
+
+let jay = "Jay's ";
+
+let local = 'es el "mejor" local';
+
+let primerTexto = 'Este texto tiene "comillas dobles". ';
+
+let segundoTexto = "Este texto tiene 'comillas simples'.";
+
+console.log (saludo);
+
+console.log ("Soy un string sin variable");
+
+console.log (bienvenida + nombre);
+
+console.log (jay + local);
+
+console.log (primerTexto + segundoTexto)
